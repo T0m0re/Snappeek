4c45354c427e012f0967ded3b50da724e58880dc
Abdulwahab Hassan <96207413+T0m0re@users.noreply.github.com>
2025-05-08 16:08:00 +0100

    mm

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,29 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+interface WebpackRule {
+  test: RegExp;
+  use: string;
+}
+
+interface WebpackConfig {
+  module: {
+    rules: WebpackRule[];
+  };
+}
+
+interface ModuleExports {
+  webpack: (config: WebpackConfig) => WebpackConfig;
+}
+
+const moduleExports: ModuleExports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.js\.map$/,
+      use: 'ignore-loader',
+    });
+    return config;
+  },
+};
+
+module.exports = moduleExports;
